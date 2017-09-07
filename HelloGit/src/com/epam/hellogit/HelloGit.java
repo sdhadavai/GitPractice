c77bb9bed405db58337e2288bc1da41a19235069
sdhadavai <sdhadavai.qa@gmail.com>
2017-09-07 11:03:09 +0530

    greeting one also updated by GitPractice

--- HelloGit/src/com/epam/hellogit/HelloGit.java
+++ HelloGit/src/com/epam/hellogit/HelloGit.java
@@ -3,7 +3,7 @@ package com.epam.hellogit;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		// greeting
+		// greeting one also updated
 		System.out.println("Hello Git");
 		System.out.println("I am developer one");
 
